476cddc4d77acfe4003db9f4d83959e7ed149bd5
Ally Morson <ally.morson@gmail.com>
2019-11-07 03:00:13 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,4 +2,5 @@ require 'nokogiri'
 require 'open-uri'
  
 doc = Nokogiri::HTML(open("https://flatironschool.com/"))
-courses = doc.css("inlineMobileLeft-2Yo002 imageTextBlockGrid2-ibtEF1").text 
+courses = doc.css("#2a778efd-1685-5ec6-9e5a-0843d6a88b7b inlineMobileLeft-2Yo002 imageTextBlockGrid2-ibtEF1")
+
